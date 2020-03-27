c8ec23c1370d1b1a2a4c48d1dfc8bd2810709134
nabeelzhr <nablz119@gmail.com>
2020-03-27 22:19:42 +0800

    implemented BUL with upper bounds and rule to only clear when full. added print statements for efficiency assessment changed to while(true) for constant running of threads changed synchronized object used blocking queue for thread safe alternative

--- src/IndexBuildingThread.java
+++ src/IndexBuildingThread.java
@@ -2,13 +2,15 @@ package UrlCrawler;
 
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 
 public class IndexBuildingThread implements Runnable{
-	private List<Url> bul;
+	private BlockingQueue<Url> bul;
 	private List<String> urlList;
-	private Set<Url> urlSet;
+	private Set<String> urlSet;
+
 	
-	public IndexBuildingThread(List<Url> bufferedList) {
+	public IndexBuildingThread(BlockingQueue<Url> bufferedList) {
 		bul = bufferedList;
 		this.urlList = Crawler.urlList;
 		this.urlSet = Crawler.urlSet;
@@ -16,35 +18,51 @@ public class IndexBuildingThread implements Runnable{
 	
 	@Override
 	public void run() {
-		Url current;
-		String currentUrl;
-		// Get first Url object
-		synchronized(Crawler.obj) {
+		while(true) {
+			Object[] tempBul;
+			String currentUrl, linkedUrl;
 			
-			while(bul.isEmpty()) {
-				try {
-					Crawler.obj.wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+			// Clear BUL if full
+			synchronized(bul) {
+				while(bul.size() != Crawler.BUL_MAX) {
+					try {
+						bul.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				tempBul =  bul.toArray();
+				bul.clear();
+				bul.notifyAll();
+			}
+			// check for duplicates; if no duplicates add to hashset and urlList
+			for(Object current : tempBul) {
+				currentUrl = ((Url)current).getUrl();
+				linkedUrl = ((Url)current).getLinkedUrl();
+				synchronized(Crawler.urlSet) {
+					if(!urlSet.contains(currentUrl)) {
+						urlSet.add(currentUrl);
+						Crawler.numUrl = urlSet.size();
+						System.out.println(currentUrl + " --> " + linkedUrl);
+						synchronized(Crawler.urlList) {
+							urlList.add(currentUrl);
+							Crawler.urlList.notifyAll();
+						}
+					}
+				}
+				long t2 = System.currentTimeMillis();
+				synchronized(Crawler.obj) {
+					if((t2 - Crawler.prevTime) >= 3600000) {
+						(Crawler.numHours)++;
+						Crawler.prevTime = t2;
+						System.out.println("current time: " + t2);
+						System.out.println(Crawler.numUrl + " Urls crawled after " + Crawler.numHours + " hours");
+					}
 				}
 			}
 			
-			current = bul.remove(0);
-
+			
 		}
-		
-		// check for duplicates; if no duplicates add to hashset and urlList
-		if((current != null) && (!urlSet.contains(current))) {
-			urlSet.add(current);
-			currentUrl = current.getUrl();
-			System.out.println(currentUrl + " --> " + current.getLinkedUrl());
-			synchronized(Crawler.obj) {
-				urlList.add(currentUrl);
-				Crawler.obj.notify();
-			}
-		}
-		
-		this.run();
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
 package UrlCrawler;
 
 import java.util.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		
 		Crawler.obj = new Object();
+		System.out.println("start time: " + Crawler.prevTime);
 		Scanner sc = new Scanner(System.in);
 		
-		List<Url> bul1 = Collections.synchronizedList(new LinkedList<Url>());
-//		List<Url> bul2 = Collections.synchronizedList(new LinkedList<Url>());
-//		List<Url> bul3 = Collections.synchronizedList(new LinkedList<Url>());
+		BlockingQueue<Url> bul1 = new LinkedBlockingQueue<Url>(1000);
+		BlockingQueue<Url> bul2 = new LinkedBlockingQueue<Url>(1000);
+		BlockingQueue<Url> bul3 = new LinkedBlockingQueue<Url>(1000);
+
+		
 		
 		// Initialize urlList with seed urls
 		while(sc.hasNextLine()){
@@ -20,13 +25,38 @@ public class Main {
 		sc.close();
 		
 		CrawlingThread ct1 = new CrawlingThread(bul1);
+		CrawlingThread ct2 = new CrawlingThread(bul1);
 		IndexBuildingThread ibt1 = new IndexBuildingThread(bul1);
-
-		Thread ct = new Thread(ct1,"CT1");
-		Thread ibt = new Thread(ibt1,"IBT1");
 		
-		ct.start();
-		ibt.start();
+		CrawlingThread ct3 = new CrawlingThread(bul2);
+		CrawlingThread ct4 = new CrawlingThread(bul2);
+		IndexBuildingThread ibt2 = new IndexBuildingThread(bul2);
+
+		CrawlingThread ct5 = new CrawlingThread(bul3);
+		CrawlingThread ct6 = new CrawlingThread(bul3);
+		IndexBuildingThread ibt3 = new IndexBuildingThread(bul3);
+
+
+		Thread tct1 = new Thread(ct1,"CT1");
+		Thread tct2 = new Thread(ct2,"CT2");
+		Thread tibt1 = new Thread(ibt1,"IBT1");
+		Thread tct3 = new Thread(ct3,"CT3");
+		Thread tct4 = new Thread(ct4,"CT4");
+		Thread tibt2 = new Thread(ibt2,"IBT2");
+		Thread tct5 = new Thread(ct5,"CT5");
+		Thread tct6 = new Thread(ct6,"CT6");
+		Thread tibt3 = new Thread(ibt3,"IBT3");
+		
+		
+		tct1.start();
+		tct2.start();
+		tibt1.start();
+		tct3.start();
+		tct4.start();
+		tibt2.start();
+		tct5.start();
+		tct6.start();
+		tibt3.start();
 		
 		
 	}
