c7e5fee223ff7048ea1e26b29ce3c78f55f9d194
nabeelzhr <nablz119@gmail.com>
2020-05-01 23:57:24 +0800

    updated crawler.jar updated README.md

--- P1_ConcurrentURLCrawler/src/Main.java
+++ P1_ConcurrentURLCrawler/src/Main.java
@@ -68,25 +68,32 @@ public class Main {
 		CrawlingThread ct2 = new CrawlingThread(bul1);
 		CrawlingThread ct3 = new CrawlingThread(bul1);
 		CrawlingThread ct4 = new CrawlingThread(bul1);
+		CrawlingThread ct5 = new CrawlingThread(bul1);
+		CrawlingThread ct6 = new CrawlingThread(bul1);
 		IndexBuildingThread ibt1 = new IndexBuildingThread(bul1);
 		
-		
-		CrawlingThread ct5 = new CrawlingThread(bul2);
-		CrawlingThread ct6 = new CrawlingThread(bul2);
 		CrawlingThread ct7 = new CrawlingThread(bul2);
 		CrawlingThread ct8 = new CrawlingThread(bul2);
+		CrawlingThread ct9 = new CrawlingThread(bul2);
+		CrawlingThread ct10 = new CrawlingThread(bul2);
+		CrawlingThread ct11 = new CrawlingThread(bul2);
+		CrawlingThread ct12 = new CrawlingThread(bul2);
 		IndexBuildingThread ibt2 = new IndexBuildingThread(bul2);
 		
-		CrawlingThread ct9 = new CrawlingThread(bul3);
-		CrawlingThread ct10 = new CrawlingThread(bul3);
-		CrawlingThread ct11 = new CrawlingThread(bul3);
-		CrawlingThread ct12 = new CrawlingThread(bul3);
+		CrawlingThread ct13 = new CrawlingThread(bul3);
+		CrawlingThread ct14 = new CrawlingThread(bul3);
+		CrawlingThread ct15 = new CrawlingThread(bul3);
+		CrawlingThread ct16 = new CrawlingThread(bul3);
+		CrawlingThread ct17 = new CrawlingThread(bul3);
+		CrawlingThread ct18 = new CrawlingThread(bul3);
 		IndexBuildingThread ibt3 = new IndexBuildingThread(bul3);
 		
-		CrawlingThread ct13 = new CrawlingThread(bul4);
-		CrawlingThread ct14 = new CrawlingThread(bul4);
-		CrawlingThread ct15 = new CrawlingThread(bul4);
-		CrawlingThread ct16 = new CrawlingThread(bul4);
+		CrawlingThread ct19 = new CrawlingThread(bul4);
+		CrawlingThread ct20 = new CrawlingThread(bul4);
+		CrawlingThread ct21 = new CrawlingThread(bul4);
+		CrawlingThread ct22 = new CrawlingThread(bul4);
+		CrawlingThread ct23 = new CrawlingThread(bul4);
+		CrawlingThread ct24 = new CrawlingThread(bul4);
 		IndexBuildingThread ibt4 = new IndexBuildingThread(bul4);
 		
 
@@ -95,24 +102,33 @@ public class Main {
 		Thread tct2 = new Thread(ct2,"CT2");
 		Thread tct3 = new Thread(ct3,"CT3");
 		Thread tct4 = new Thread(ct4,"CT4");
-		Thread tibt1 = new Thread(ibt1,"IBT1");
-		
 		Thread tct5 = new Thread(ct5,"CT5");
 		Thread tct6 = new Thread(ct6,"CT6");
+		Thread tibt1 = new Thread(ibt1,"IBT1");
+		
+	
 		Thread tct7 = new Thread(ct7,"CT7");
 		Thread tct8 = new Thread(ct8,"CT8");
-		Thread tibt2 = new Thread(ibt2,"IBT2");
-		
 		Thread tct9 = new Thread(ct9,"CT9");
 		Thread tct10 = new Thread(ct10,"CT10");
 		Thread tct11 = new Thread(ct11,"CT11");
 		Thread tct12 = new Thread(ct12,"CT12");
-		Thread tibt3 = new Thread(ibt3,"IBT3");
+		Thread tibt2 = new Thread(ibt2,"IBT2");
 		
 		Thread tct13 = new Thread(ct13,"CT13");
 		Thread tct14 = new Thread(ct14,"CT14");
 		Thread tct15 = new Thread(ct15,"CT15");
 		Thread tct16 = new Thread(ct16,"CT16");
+		Thread tct17 = new Thread(ct17,"CT17");
+		Thread tct18 = new Thread(ct18,"CT18");
+		Thread tibt3 = new Thread(ibt3,"IBT3");
+		
+		Thread tct19 = new Thread(ct19,"CT19");
+		Thread tct20 = new Thread(ct20,"CT20");
+		Thread tct21 = new Thread(ct21,"CT21");
+		Thread tct22 = new Thread(ct22,"CT22");
+		Thread tct23 = new Thread(ct23,"CT23");
+		Thread tct24 = new Thread(ct24,"CT24");
 		Thread tibt4 = new Thread(ibt4,"IBT4");
 		
 	
@@ -122,24 +138,32 @@ public class Main {
 		tct2.start();
 		tct3.start();
 		tct4.start();
-		tibt1.start();
-		
 		tct5.start();
 		tct6.start();
+		tibt1.start();
+		
 		tct7.start();
 		tct8.start();
-		tibt2.start();
-		
 		tct9.start();
 		tct10.start();
 		tct11.start();
 		tct12.start();
-		tibt3.start();
+		tibt2.start();
 		
 		tct13.start();
 		tct14.start();
 		tct15.start();
 		tct16.start();
+		tct17.start();
+		tct18.start();
+		tibt3.start();
+		
+		tct19.start();
+		tct20.start();
+		tct21.start();
+		tct22.start();
+		tct23.start();
+		tct24.start();
 		tibt4.start();
 		
 		
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-"URL-Crawler" 
+# Concurrent Web Crawler
+
+The project involves the use of concuurency to efficiently crawl URLs and storing the HTML files of the found URLs in a local directory. The data race issue of having HTMLs from duplicate URLs stored is also prevented using synchronization mechanisms and an efficient indexing system to check for duplicates.
+
+A merger jar file will not be necessary in this case as the program is designed to populate the results findings as the program runs.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+The only requirement prior to executing the programs would be to set up the required jar file and the seed URL file to be taken as input.
+
+1. Under the P1_ConcurrentURLCrawler/jar/ folder, extract the crawler.jar file.
+2. Ensure that the seed url input file is named to seed.txt.
+3. Place the compiled crawler.jar file along with the seed.txt file into an empty directory or local folder.
+
+## Execution
+The execution of the crawler.jar file will cause additional folder to be created in the selected directory.
+The following command is executed to run the program for 24h , taking input from the seed.txt file and output results of crawled URLs to the res.txt file. The maximum number of HTMLs stored is indicated by the storedPageNum field. In this case, only 1000 HTML files will be stored in local directory.
+
+```
+java -jar crawler.jar -input seed.txt -output res.txt -time 24h -storedPageNum 1000
+```
+
+## Results
+The list of crawled URLs will be indicated in the res.txt file while the HTMLs will be stored in the folder, html/.
+
+
+
+
